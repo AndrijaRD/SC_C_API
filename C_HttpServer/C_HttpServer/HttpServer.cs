2a0813933fd0ab5615ff4ab922512808cc3555c3
AndrijaRD <55910028+AndrijaRD@users.noreply.github.com>
2023-02-04 10:03:39 +0100

    upload

---
+++ C_HttpServer/C_HttpServer/HttpServer.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+namespace C_HttpServer
+{
+    public class HttpServer
+    {
+        public const String MSG_DIR = "/root/msg/";
+        public const String WEB_DIR = "/root/web/";
+        public const String VERSION = "HTTP/1.1";
+        public const String NAME = "MatthiWare HTTP Server v0.1";
+
+        private bool running = false;
+        private readonly TcpListener listener;
+
+
+        public HttpServer(int port) {
+            listener = new TcpListener(IPAddress.Any, port);
+        }
+
+        public void Start() {
+            Thread serverThread = new(new ThreadStart(Run));
+            serverThread.Start();
+        }
+
+        private void Run()
+        {   
+            running = true;
+            listener.Start();
+
+            while (running) {
+                Console.WriteLine("\nWaiting for connection...");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client connected!");
+
+                HandleClient(client);
+
+                client.Close();
+            }
+
+            running = false;
+            listener.Stop();
+        }
+
+        private static void HandleClient(TcpClient client)
+        {
+            StreamReader reader = new(client.GetStream());
+
+            string msg = "";
+            while (reader.Peek() != -1)
+                msg += reader.ReadLine() + "\n\t";
+
+            Console.WriteLine("REQUEST: \n\t" + msg);
+
+            Request req = Request.GetRequest(msg);
+            Response resp = Response.From(req);
+            resp.Post(client.GetStream());
+
+        }
+
+    }
+}
---
+++ C_HttpServer/C_HttpServer/Request.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_HttpServer
+{
+    public class Request
+    {
+        public string Type { get; set; }
+        public string URL { get; set; }
+        public string Host { get; set; }
+
+        public String Referer { get; set; }
+
+        private Request(String type, String url, String host, String referer) 
+        {
+            Type= type;
+            URL= url;
+            Host= host;
+            Referer=referer;
+        }
+
+        public static Request GetRequest(String requst) {
+
+            if (String.IsNullOrEmpty(requst))
+                return null;
+
+            String[] tokens = requst.Split(' ');
+            String type = tokens[0];
+            String url = tokens[1];
+            String host = tokens[4];
+            String referer = "";
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i] == "Referer:")
+                {
+                    referer = tokens[i + 1];
+                    break;
+                }
+            }
+
+            return new Request(type, url, host, referer);
+        }
+    }
+}
